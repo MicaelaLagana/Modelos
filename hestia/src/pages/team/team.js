6d3f86341ec7eeb03fa9d1a37cee91a3a213e478
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2021-11-09 22:15:45 -0300

    actualizacion fotos team

--- hestia/src/pages/team/team.js
+++ hestia/src/pages/team/team.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Navbar from "../../components/navbar";
 import { Grid } from '@material-ui/core'
-import anubis from  "../../assets/images/anubis.jpg";
-import hathor from  "../../assets/images/hathor.jpg";
-import horus from  "../../assets/images/horus.jpg";
-import thot from  "../../assets/images/thot.jpg";
+import fede from  "../../assets/images/Federico_Santos.jpg";
+import mica from  "../../assets/images/Micaela_Lagana.jpg";
+import chris from  "../../assets/images/Christian_Giliberti.jpg";
+import nes from  "../../assets/images/Nestor_Rivera.jpg";
 
 export default function Team() {
   return (
     <>
       <Navbar />
-      <h2>Nuesto equipo</h2>
+      <div className='titulo'>
+      <h2>Nuestro Equipo</h2>
+      </div>
+      
       <div className='teamgrid'>
         <Grid container spacing={2}>
           <Grid item xs={2} />
@@ -19,13 +22,13 @@ export default function Team() {
               <div className='card-inner'>
                 <div className='card-front'>
                   <div>
-                    <img className='teamimg' src={anubis}/>
+                    <img className='teamimg' src={fede}/>
                   </div>
                   <li>
                     <strong>Federico Santos</strong>
                   </li>
                   <li>
-                    <strong>Programador</strong>
+                    <strong>Software Developer</strong>
                   </li>
                 </div>
                 <div className='card-back'>
@@ -39,19 +42,18 @@ export default function Team() {
               </div>
             </div>
           </Grid>
-
           <Grid item xs={4}>
             <div className='card'>
               <div className='card-inner'>
                 <div className='card-front'>
                 <div>
-                    <img className='teamimg' src={hathor}/>
+                    <img className='teamimg' src={mica}/>
                   </div>
                   <li>
                     <strong>Micaela Lagana</strong>
                   </li>
                   <li>
-                    <strong>Programador</strong>
+                    <strong>Software Developer</strong>
                   </li>
                 </div>
                 <div className='card-back'>
@@ -72,7 +74,7 @@ export default function Team() {
               <div className='card-inner'>
                 <div className='card-front'>
                 <div>
-                    <img className='teamimg' src={horus}/>
+                    <img className='teamimg' src={chris}/>
                   </div>
                   <li>
                     <strong>Christian Giliberti</strong>
@@ -97,7 +99,7 @@ export default function Team() {
               <div className='card-inner'>
                 <div className='card-front'>
                 <div>
-                    <img className='teamimg' src={thot}/>
+                    <img className='teamimg' src={nes}/>
                   </div>
                   <li>
                     <strong>Nestor Rivera</strong>
